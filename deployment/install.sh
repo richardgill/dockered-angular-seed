8ad1e30a109c21d7136efdbcd7892e1445ce9af5
Richard Gill <peenuty@gmail.com>
2014-07-19 08:43:40 +0100

    Deleted unneeded deployment config.

--- deployment/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /tmp/
-git clone git@bitbucket.org:rgill/garner-webui.git
-cd /tmp/garner-webui/web/
-npm install
-bower install --allow-root
-grunt
-mv /tmp/garner-webui/web/app/* /var/www/garner-webui/
--- deployment/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/opt/nginx/sbin/nginx -c /opt/nginx/conf/nginx.conf && tail -1000f /var/log/nginx/nginx_error.log
