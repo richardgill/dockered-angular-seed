7624bf7ee53156d3d253a7f8dcec37aa1ca97ecf
Richard Gill <peenuty@gmail.com>
2014-07-19 12:36:17 +0100

    Files which aren't needed.

--- web/scripts/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-BASE_DIR=`dirname $0`
-
-echo ""
-echo "Starting Karma Server (https://github.com/karma-runner/karma)"
-echo "-------------------------------------------------------------------"
-
-karma start $BASE_DIR/../config/karma.conf.coffee $*
