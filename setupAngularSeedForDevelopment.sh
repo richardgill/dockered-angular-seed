4a6ab260782f33c430e1e3f1bf20797d0e450a8c
Richard Gill <richard@rgill.co.uk>
2014-08-10 12:07:49 +0100

    Made grunt serve run automatically.

--- setupAngularSeedForDevelopment.sh
+++ setupAngularSeedForDevelopment.sh
@@ -26,7 +26,7 @@
 
 SCRIPT
 
-sudo docker run -v $PWD:/mount/angular-webui/ -p 9000:9000 -p 9876:9876 -p 4444:4444 -p 35729:35729 -e IP=$(hostname -I | awk '{print $1}') -i -t angular-webui bash
+sudo docker run -v $PWD:/mount/angular-webui/ -p 9000:9000 -p 9876:9876 -p 4444:4444 -p 35729:35729 -e IP=$(hostname -I | awk '{print $1}') -i -t angular-webui bash -c "cd /mount/angular-webui/ && ln -s /tmp/node_modules . && ln -s /tmp/bower_components . && scripts/startXServerForFirefox.sh && npm install && bower install --allow-root && scripts/setupE2ETests.sh && grunt serve"
 
 
 }
